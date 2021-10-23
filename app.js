96c61c55efa7dd7d48cdc36e7a5509ba159bcd05
Hiram Chavez <virtual.liga@gmail.com>
2021-10-23 13:07:28 -0500

    Se importa y usa CORS en el APP.JS

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+const cors = require('cors');
+
+app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
